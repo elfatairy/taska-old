44e5f1072d7dcc326e613f1ae79b38563e672e2f
OmarHassan <omar13102005@gmail.com>
2025-10-08 15:50:28 +0300

    fix: change min height of the content to lvh instead of svh

--- app/components/ui/sidebar.tsx
+++ app/components/ui/sidebar.tsx
@@ -142,7 +142,7 @@ function SidebarProvider({
             } as React.CSSProperties
           }
           className={cn(
-            "group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-svh w-full",
+            "group/sidebar-wrapper has-data-[variant=inset]:bg-sidebar flex min-h-lvh w-full",
             className
           )}
           {...props}
